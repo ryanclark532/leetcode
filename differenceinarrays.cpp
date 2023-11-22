da5addbab444fe6e276a7372f080682f5a65b88a
Ryan Clark <clark0859@gmail.com>
2023-11-22 20:51:13 +1100

    Added 2215. Find the Difference of Two Arrays

---
+++ differenceinarrays.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+vector<vector<int>> findDifferences(vector<int> nums1, vector<int> nums2)
+{
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> set2(nums2.begin(), nums2.end());
+
+    vector<int> distinct_nums1, distinct_nums2;
+    for (int num : set1)
+    {
+        if (set2.count(num) == 0)
+        {
+            distinct_nums1.push_back(num);
+        }
+    }
+
+    for (int num : set2)
+    {
+        if (set1.count(num) == 0)
+        {
+            distinct_nums2.push_back(num);
+        }
+    }
+
+    return {distinct_nums1, distinct_nums2};
+}
+
+int main()
+{
+    vector<vector<int>> res = findDifferences({1, 2, 3}, {2, 4, 4});
+    for (int i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+    }
+    return 0;
+}
